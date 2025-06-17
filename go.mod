d63ffb402bcb5cc3cc055ecdea84ad3fe6719b9a
nurgal1ev <nurgaliev0644@gmail.com>
2025-06-17 20:44:56 +0300

    variables and ifelse

--- go.mod
+++
@@ -1,3 +0,0 @@
-module foo/bar
-
-go 1.24.4
--- main.go
+++ main.go
@@ -3,6 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	name := "golang"
-	fmt.Println("Hello World")
+	score := 18
+	/*
+		if score > 15 {
+			if score > 15 {
+				fmt.Println("great")
+			} else {
+				fmt.Println("good")
+			}
+		} else {
+			fmt.Println("bad")
+		}*/
+
+	if score > 15 {
+		fmt.Println("great")
+	} else if score > 10 {
+		fmt.Println("good")
+	} else {
+		fmt.Println("bad")
+	}
 }
